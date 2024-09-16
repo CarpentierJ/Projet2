9103f5882a9411bdf4672957b1126cdc7a44dffb
imatrix04 <florent.germain040105@orange.fr>
2024-09-16 16:03:44 +0200

    save 4.0

---
+++ test-dossier/testaccueil.js
@@ -0,0 +1,28 @@
+//----------------------------Déconnexion-------------------------------//
+
+document.getElementById("logout-button").addEventListener("click", function() {
+    // Supprimer le token du localStorage
+    localStorage.removeItem('token');
+    
+    // Optionnel : Supprimer les autres données de connexion comme les tentatives de connexion
+    localStorage.removeItem('loginAttempts');
+    localStorage.removeItem('blockTime');
+
+    // Message de confirmation
+    alert('Vous avez été déconnecté.');
+
+    // Rediriger l'utilisateur vers la page de connexion ou d'accueil
+    window.location.href = "../index.html";
+});
+
+//----------------------------Vérification de connexion-------------------------------//
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Vérifier si le token est présent dans le localStorage
+    const token = localStorage.getItem('token');
+    
+    // Si le token n'est pas présent, rediriger vers la page de connexion
+    if (!token) {
+        window.location.href = "../index.html"; // Rediriger vers la page de connexion
+    }
+});
